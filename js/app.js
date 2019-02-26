7f2ebe1f88b4a3a0cf274e89c817bc3e392b645d
gittableauextensions <45185401+gittableauextensions@users.noreply.github.com>
2019-02-25 16:30:50 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -64,7 +64,7 @@ function startTimer(duration, display) {
 }
 
 window.onload = function () {
-    var fiveMinutes = 60 * 1,
+    var fiveMinutes = 60 * 2,
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
 };
